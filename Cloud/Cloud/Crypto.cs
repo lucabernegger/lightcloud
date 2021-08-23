a9304c1adcf54fd3bc5420ef87ad464460c25af4
Luca <luca.bernegger@gmail.com>
2021-08-23 18:47:43 +0200

    update Crypt,o, remove dep

--- Cloud/Cloud/Crypto.cs
+++ Cloud/Cloud/Crypto.cs
@@ -7,34 +7,9 @@ namespace Cloud
     {
         public static byte[] EncryptByteArray(byte[] key, byte[] secret)
         {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                using (AesManaged cryptor = new AesManaged())
-                {
-                    cryptor.Mode = CipherMode.CBC;
-                    cryptor.Padding = PaddingMode.PKCS7;
-                    cryptor.KeySize = 128;
-                    cryptor.BlockSize = 128;
-
-                    //We use the random generated iv created by AesManaged
-                    byte[] iv = cryptor.IV;
-
-                    using (CryptoStream cs = new CryptoStream(ms, cryptor.CreateEncryptor(key, iv), CryptoStreamMode.Write))
-                    {
-                        cs.Write(secret, 0, secret.Length);
-                    }
-                    byte[] encryptedContent = ms.ToArray();
-
-                    //Create new byte array that should contain both unencrypted iv and encrypted data
-                    byte[] result = new byte[iv.Length + encryptedContent.Length];
-
-                    //copy our 2 array into one
-                    System.Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
-                    System.Buffer.BlockCopy(encryptedContent, 0, result, iv.Length, encryptedContent.Length);
-
-                    return result;
-                }
-            }
+            using Aes aes = Aes.Create();
+            aes.Key = key;
+            return aes.EncryptCbc(secret,aes.IV);
         }
 
         /// <summary>
@@ -45,30 +20,10 @@ namespace Cloud
         /// <returns>decrypted bytes</returns>
         public static byte[] DecryptByteArray(byte[] key, byte[] secret)
         {
-            byte[] iv = new byte[16]; //initial vector is 16 bytes
-            byte[] encryptedContent = new byte[secret.Length - 16]; //the rest should be encryptedcontent
+            using Aes aes = Aes.Create();
+            aes.Key = key;
 
-            //Copy data to byte array
-            System.Buffer.BlockCopy(secret, 0, iv, 0, iv.Length);
-            System.Buffer.BlockCopy(secret, iv.Length, encryptedContent, 0, encryptedContent.Length);
-
-            using (MemoryStream ms = new MemoryStream())
-            {
-                using (AesManaged cryptor = new AesManaged())
-                {
-                    cryptor.Mode = CipherMode.CBC;
-                    cryptor.Padding = PaddingMode.PKCS7;
-                    cryptor.KeySize = 128;
-                    cryptor.BlockSize = 128;
-
-                    using (CryptoStream cs = new CryptoStream(ms, cryptor.CreateDecryptor(key, iv), CryptoStreamMode.Write))
-                    {
-                        cs.Write(encryptedContent, 0, encryptedContent.Length);
-
-                    }
-                    return ms.ToArray();
-                }
-            }
+            return aes.DecryptCbc(secret,aes.IV);
         }
     }
 }
